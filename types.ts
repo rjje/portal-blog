7944e7e29c3a7b03b83bd0acc2dabc6b6c211832
rjje <48533355+rjje@users.noreply.github.com>
2025-09-25 01:12:40 +0800

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,26 @@
+export interface Post {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  tags: string[];
+  category: string[];
+  image: string;
+  author: string;
+}
+
+export interface ReadingPost {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  tag: string;
+  category: string;
+  image: string;
+  author: string;
+}
+
+export interface TypePages {
+  title: string;
+  slug: string;
+}
